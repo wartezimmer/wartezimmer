e8e61ddcaa0ee065364ae73c3969e8ac9b636f9b
Sebastian Herrlinger <hasta84@gmail.com>
2020-03-21 19:14:22 +0100

    setup pg initial schema migration

---
+++ migrations/1584812166404_initial-schema.js
@@ -0,0 +1,64 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.createTable('users', {
+    id: 'id',
+    userId: { 
+      type: 'varchar(128)', 
+      notNull: true 
+    },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+
+  pgm.createTable('queues', {
+    id: 'id',
+    userId: { 
+      type: 'varchar(128)', 
+      notNull: true 
+    },
+    facilityId: { 
+      type: 'id',
+      references: '"facilities"',
+      onDelete: 'cascade',
+      notNull: true
+    },
+    earliestDeparture: { 
+      type: 'timestamp',
+      notNull: true 
+    },
+    travelTime: {
+      type: 'integer',
+      notNull: true
+    },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+
+  pgm.createTable('facilities', {
+    id: 'id',
+    externalId: { 
+      type: 'varchar(128)', 
+      notNull: true 
+    },
+    name: { 
+      type: 'varchar(128)', 
+      notNull: true 
+    },
+    createdAt: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+};
+
+exports.down = pgm => {};
