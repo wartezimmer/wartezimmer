32724b2225caf6ff5c64d7975c84b214b7a4134c
ksollner <ksollner@feinarbyte.de>
2020-03-21 19:17:42 +0100

    routes

--- packages/api/server.js
+++ packages/api/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-
+const facility = require("./lib/controllers/facility");
 const app = express()
 
 app.get('/', (req, res) => {
     res.end('hello wartezimmer')
 })
-
+app.use("/facility", facility)
 app.listen(process.env.PORT || 3001)
