647f8082a8fa2c556c170e6136362a7d6975e72e
Bernd Kaiser <bk@dfjk.eu>
2020-03-22 19:15:21 +0100

    facilities nearest

--- packages/api/src/lib/controllers/facilities.ts
+++ packages/api/src/lib/controllers/facilities.ts
@@ -6,3 +6,24 @@ facilitiesRouter.get('/', (req, res) => {
     var facilityId = req.params["id"]
     // todo
 })
+
+// FIXME adjust column names
+// I used different names from a local postgres instance (I did not get admin-panel working)
+export const FACILITIES_NEAREST_QUERY = `
+select
+	name,
+	object_id,
+	address_city,
+	earth_distance(ll_to_earth(f.latitude,
+	f.longitude),
+	ll_to_earth($1,
+	$2))
+from
+	facilities2 f
+order by
+	earth_distance(ll_to_earth(f.latitude,
+	f.longitude),
+	ll_to_earth($1,
+	$2))
+limit 100;
+`;
