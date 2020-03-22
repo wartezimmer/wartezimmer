770802a8b9c8b59fa1dec6a825821cad48258c3d
ksollner <ksollner@feinarbyte.de>
2020-03-22 16:25:54 +0100

    test search results

--- packages/frontend/src/state/thunks/fetchFacilities.ts
+++ packages/frontend/src/state/thunks/fetchFacilities.ts
@@ -5,9 +5,16 @@ export function fetchFacilities(search: string) {
     return async (dispatch: ReduxDispatch) => {
         // const result = await (await fetch("/backend/")).json();
         dispatch(AppApi.setCurrentSearchResult([
-            // {
-                
-            // }
+            {
+                current_load: 0,
+                email: "test@test.de",
+                id: "1234",
+                load: {},
+                name: "Testklinik",
+                phone: "+49111",
+                street: "test str. 10",
+                website: "test.de",
+            }
         ]));
     };
 }
