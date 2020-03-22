39e5fab6dd7c6cea4b59daeccffdf62e4528d58d
Sebastian Herrlinger <hasta84@gmail.com>
2020-03-22 12:57:52 +0100

    setup admin with ejs

--- packages/admin-panel/server.js
+++ packages/admin-panel/server.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const path = require('path')
 const pgClient = require('shared-lib/lib/pg-client')
 
 const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 const startup = async () => {
   const { db } = await pgClient()
 
   app.get('/', (req, res) => {
-    res.end('hello wartezimmer admin panel')
+    res.render('index');
+  })
+
+  app.get('/update-facilities', (req, res) => {
+    res.render('update-facilities', { foo: 'FOO' });
   })
 
   app.listen(process.env.PORT || 3003)
 }
-startup()
+startup() 
