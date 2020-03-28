37ca8ea5403d33af5abe588922e1f613231cd147
Leon Schmidt <le.sch@live.de>
2020-03-29 00:14:08 +0100

    improve search

--- packages/frontend/src/components/MapSearchResultList.tsx
+++ packages/frontend/src/components/MapSearchResultList.tsx
@@ -1,5 +1,5 @@
 import { List } from "antd";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 
 import { State } from "../state";
@@ -9,26 +9,39 @@ import { useThunkDispatch } from "../useThunkDispatch";
 export const MapSearchResultList = () => {
     const dispatch = useThunkDispatch();
     const searchResult = useSelector((state: State) => state.app.currentSearchResult);
-    
+    const scroll = useRef(null) as any;
+    const halfWay = useRef(null);
+
     if (searchResult === null || !searchResult.length) {
         return null;
     }
-    
+    useEffect(() => {
+        if (halfWay != undefined) {
+            console.log(scroll);
+            let element = halfWay as any;
+            scroll.current.scrollTo(0, element.current.offsetTop);
+        }
+    });
     return (
         <>
-            <List className="map-results" >
-                {searchResult.map((r, n) => (
-                    <List.Item onClick={() => {
-                        dispatch(AppApi.setCurrentFacility(r))
-                    }} key={`searchResult${n}`}>
-                        <List.Item.Meta
-                            title={<a >{r.name}</a>}
-                            description={`${r.address_street}`}
-                        />
-                        {/* <div>Todo: Auslastung</div> */}
-                    </List.Item>
-                ))}
-            </List>
+            <div ref={scroll} className="results">
+                <div className="top"></div>
+                <div ref={halfWay} className="nothing"></div>
+                <List className="map-results">
+                    <div className="pill"></div>
+                    {searchResult.map((r, n) => (
+                        <List.Item
+                            onClick={() => {
+                                dispatch(AppApi.setCurrentFacility(r));
+                            }}
+                            key={`searchResult${n}`}
+                        >
+                            <List.Item.Meta title={<a>{r.name}</a>} description={`${r.address_street}`} />
+                            {/* <div>Todo: Auslastung</div> */}
+                        </List.Item>
+                    ))}
+                </List>
+            </div>
         </>
     );
 };
