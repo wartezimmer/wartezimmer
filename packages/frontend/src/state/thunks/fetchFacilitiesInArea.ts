7fd301f9bb8f2f8601fc77dc73920205f24d4688
Sebastian Herrlinger <hasta84@gmail.com>
2020-03-30 21:16:17 +0200

    debounce fetch facilities in area (#41)

--- packages/frontend/src/state/thunks/fetchFacilitiesInArea.ts
+++ packages/frontend/src/state/thunks/fetchFacilitiesInArea.ts
@@ -1,17 +1,21 @@
 import { ReduxDispatch } from "../../useThunkDispatch";
 import { AppApi, backend_url } from "../app";
 
+let timeout:any = null
+
 export function fetchFacilitiesInArea() {
     return async (dispatch: ReduxDispatch, getState) => {
-        const { currentArea } = getState().app;
-        const params = new URLSearchParams(currentArea).toString();
-        const response = await fetch(`${backend_url}/facilities/area?${params}`);
-        const json = await response.json();
+        clearTimeout(timeout)
+        timeout = setTimeout(async () => {
+            const { currentArea } = getState().app;
+            const params = new URLSearchParams(currentArea).toString();
+            const response = await fetch(`${backend_url}/facilities/area?${params}`);
+            const json = await response.json();
 
-        if (json.status === 'success') {
-            dispatch(AppApi.setFacilities(json.result));
-        } 
-        
-        return json
+            if (json.status === 'success') {
+                dispatch(AppApi.setFacilities(json.result));
+            } 
+            // TODO: handle and report error
+        }, 300)
     };
 }
