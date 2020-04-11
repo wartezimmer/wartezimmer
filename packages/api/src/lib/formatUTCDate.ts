a53aad9ffe2a66653a0d20e31ace919e828ad8ea
Sebastian Herrlinger <hasta84@gmail.com>
2020-04-11 22:54:16 +0200

    fix api ts build (#61)

--- packages/api/src/lib/formatUTCDate.ts
+++ packages/api/src/lib/formatUTCDate.ts
@@ -1,4 +1,4 @@
-function formatUTCDate(date) {
+export function formatUTCDate(date) {
   let month = date.getUTCMonth() + 1;
   let day = date.getUTCDate();
   const year = date.getUTCFullYear();
@@ -13,10 +13,6 @@ function formatUTCDate(date) {
   return `${year}-${month}-${day}`;
 }
 
-const oneDay = 1000 * 60 * 60 * 24;
+export const oneDay = 1000 * 60 * 60 * 24;
 
-const formatNowMinusDays = (days) => formatUTCDate(new Date(Date.now() - oneDay * Math.abs(days)))
-
-module.exports = {
-  formatNowMinusDays
-}
+export const formatNowMinusDays = (days) => formatUTCDate(new Date(Date.now() - oneDay * Math.abs(days)))
