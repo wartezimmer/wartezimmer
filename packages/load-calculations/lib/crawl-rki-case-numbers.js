b95d0948bbaa7623fde2f652009e9f3a020f7b5e
Sebastian Herrlinger <hasta84@gmail.com>
2020-04-17 22:26:16 +0200

    precalcualate new cases per day

--- packages/load-calculations/lib/crawl-rki-case-numbers.js
+++ packages/load-calculations/lib/crawl-rki-case-numbers.js
@@ -14,7 +14,20 @@ async function crawlRKICaseNumbers(db, url, extractionRunTime = (new Date()).toU
         run_time: extractionRunTime
     }))
     await db('rki_district_case_numbers').insert(data);
-
+    await db.raw(`
+        update rki_district_case_numbers
+        set new_cases = (rki_district_case_numbers.cases - b.cases)
+        from rki_district_case_numbers as b 
+        where rki_district_case_numbers."OBJECTID" = b."OBJECTID"
+        and b.run_time = (
+            SELECT MAX(c.run_time) 
+            FROM rki_district_case_numbers as c
+            WHERE c.run_time::date < rki_district_case_numbers.run_time::date
+            and c."OBJECTID" = rki_district_case_numbers."OBJECTID"
+        )
+        and rki_district_case_numbers.run_time = ?
+    `, [extractionRunTime])
+    
     logger.info(`Updated RKI district case numbers with ${data.length} entries`)
 }
 
